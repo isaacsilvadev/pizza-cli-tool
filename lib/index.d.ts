6e81b82e96772a0534a80a49657a7b1d831ebfb8
isaacsilva-cit <isaacsilva@ciandt.com>
2023-03-01 19:38:39 -0300

    pizza cli

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+declare const chalk: any;
+declare const clear: any;
+declare const program: any;
+declare const fs: any;
+declare const path: any;
+declare const figlet: any;
+declare const cheese: string;
